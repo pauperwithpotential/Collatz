2ed10efc65940b5b9a03d7542d555c3b27439a15
pauperwithpotential <pauperwithpotential@users.noreply.github.com>
2017-06-22 17:21:43 +0800

    Add files via upload

--- collatz.py
+++ collatz.py
@@ -2,27 +2,27 @@
 Write function name collatz() with one parameter, number
 If number is even, print and return number // 2
 If number is odd, print and return 3 * number + 1
+Let user type in an integer that keeps calling collatz() till it returns the value 1
+Add try and except statements to detect whether user types in a non-integer string
 '''
 
 def collatz(number):
-        newNumber = 0
         if number % 2 == 0:
-            newNumber = number // 2
+            number = number // 2
             
         elif number % 2 == 1:
-            newNumber = 3 * number + 1
+            number = 3 * number + 1
 
-        print(newNumber)
+        print(number)
     
-        if newNumber > 1:
-            collatz(newNumber)
-
+        if number > 1:
+            collatz(number)        
 
 while True:
-    print('Enter a number:')
-    number = input()
     try:
-        print(collatz(int(number)))
+        print('Enter any number:')
+        number = int(input())
+        collatz(number)
     except ValueError:
         print('Must enter an integer')
 
